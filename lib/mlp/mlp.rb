c74a9c44994b35ad902ec9a19feb067529b41238
Jamison Dance <jergason@gmail.com>
2011-10-12 15:46:53 -0600

    initial commit

--- lib/mlp/mlp.rb
+++ lib/mlp/mlp.rb
@@ -1,35 +1,58 @@
 module MLP
   class MLP
-    # Takes an array of the form [input_nodes, number_of_hidden_nodes_for_hidden_layer . . ., number_of_output_nodes].
-    def initialize(arr)
-      #store weights as a 2d array
-      raise ArgumentError.new('Input must be an array of at least length 3') unless arr.instance_of? Array
-      @input_nodes = arr[0]
-      @hidden_layers = arr[1...-1]
-      @output_nodes = arr[-1]
+    def initialize(opts)
+      opts[:initial_weight] ||= 0.5
+      opts[:learning_rate] ||= 0.2
+      opts[:threshold_function] ||= Proc.new { |net| 1 / (1 + Math.exp(-net)) }
 
-      @threshold_function = Proc.new { |net| 1 / (1 + Math.exp(-net)) }
-      #build the network
-      build_network
+      input_nodes = opts[:input]
+      #hidden layers = an array of number of nodes per layer, or just a number of layers
+      # and we will choose how many nodes per layer?
+      hidden_layers = opts[:hidden_layers]
+      output_nodes = opts[:output]
+      @learning_rate = opts[:learning_rate]
+      @threshold_function = opts[:threshold_function]
+      initial_weight = opts[:initial_weight]
+
+      #initialize weights matrix
+      @weights = []
+      #intialize matrix from inputs to hidden layers
+      input_to_hidden = []
+      number_of_nodes_in_first_hidden_layer = hidden_layers.shift
+      input_nodes.times do
+        input = []
+        number_of_nodes_in_first_hidden_layer.times { input << initial_weight }
+        input_to_hidden << input
+      end
+      @weights << input_to_hidden
+
+
+      #initialize hidden layers
+      hidden_layers.each do |number_of_output_nodes|
+        number_of_input_nodes = @weights[-1][0].count
+        hidden_layer = []
+        number_of_input_nodes.times do
+          input = []
+          number_of_output_nodes.times { input << initial_weight }
+          hidden_layer << input
+        end
+        @weights << hiddden_layer
+      end
+
+
+      #initialize last hidden layer to output
+
     end
 
-    def build_network
-      input_layer = []
-      @input_nodes.times do
-        input_layer << MLP::Node.new(@threshold_function, [0.5])
+    # Given an array of inputs and a class, update the weights
+    # of the mpl.
+    def train(input, class_for_input)
+      #compute output - output = input * weight
+      intermediate = 0
+      @weights.each_with_index do |input, i|
+        out
       end
-      @input_nodes = input_layer
-
-
-
-      hidden_layers = []
-      num_nodes_in_first_hidden_layer = @hidden_layers.unshift
-      #is this the best way to do it? Every node in one layer feeds in to the next layer?
-      @hidden_layers.each do |number_of_hidden_nodes|
-        if layer == 0
-
-        end
-      end
+      #how to do this?
     end
   end
 end
